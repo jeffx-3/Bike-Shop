1b40e02e3eeb29bedcad05acd0efdd6bb0b2e5ec
jeffx-3 <geoffreygroovy@gmail.com>
2024-04-12 21:08:16 +0300

    created files added transition to the image switch 0.3s added click functionalty to the colour picker buttons

---
+++ script.js
@@ -0,0 +1,13 @@
+let redBtn= document.getElementById("red");
+let blackBtn= document.getElementById("black");
+let blueBtn= document.getElementById("blue");
+let bike= document.getElementById("bike");
+redBtn.onclick= function(){
+bike.style.backgroundImage= "url(#)";
+}
+blackBtn.onclick= function(){
+    bike.style.backgroundImage= "url(#)";
+    }
+    blueBtn.onclick= function(){
+        bike.style.backgroundImage= "url(#)";
+        }
