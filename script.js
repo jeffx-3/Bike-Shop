c3c3ad8cbef3f116ff79a25ae5812546cea8f47f
jeffx-3 <geoffreygroovy@gmail.com>
2024-04-16 09:00:16 +0300

    added images and logo

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let blackBtn= document.getElementById("black");
 let blueBtn= document.getElementById("blue");
 let bike= document.getElementById("bike");
 redBtn.onclick= function(){
-bike.style.backgroundImage= "url(#)";
+bike.style.backgroundImage= "url(media/erkka-wessman-nKhJCl4AgPU-unsplash.jpg)";
 }
 blackBtn.onclick= function(){
-    bike.style.backgroundImage= "url(#)";
+    bike.style.backgroundImage= "url(media/jakub-sisulak-GqfRtLnGpmI-unsplash.jpg)";
     }
     blueBtn.onclick= function(){
-        bike.style.backgroundImage= "url(#)";
+        bike.style.backgroundImage= "url(media/max-itin-7GeprSfVqLQ-unsplash.jpg)";
         }
